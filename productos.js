01eadf82b7bfabcfee71e81f70730d90393ec2da
armotard <aitorubio034@gmail.com>
2026-07-23 19:45:33 +0200

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,18 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "AR BRAKE DUCT",
+    precio: 39.99,
+    imagen: "images/ar-brake-duct.png",
+    categoria: "AR FACTORY · BRAKE PERFORMANCE",
+    descripcion:
+      "Conducto de refrigeración para la pinza de freno trasera, diseñado para mejorar el flujo de aire y ayudar a reducir la acumulación de temperatura durante un uso intensivo.",
+    detalles: [
+      "Compatible con cualquier moto",
+      "Diseño compacto",
+      "Diseñado para Supermoto"
+    ]
+    enlaceCompra:
+      "https://es.wallapop.com/item/llavero-giratorio-con-rodamiento-antiestres-1257879271"
+  }
+];
