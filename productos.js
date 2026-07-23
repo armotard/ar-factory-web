cc96fd5ceb490b4b416f791a3c174571b5a72bcb
armotard <aitorubio034@gmail.com>
2026-07-23 20:05:51 +0200

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,31 +1,33 @@
 const productos = [
+
   {
-    id: 1
+    id: 1,
     nombre: "AR BRAKE DUCT",
     precio: 39.99,
     imagen: "images/ar-brake-duct.png",
     categoria: "AR FACTORY · BRAKE PERFORMANCE",
-    descripcion: "Conducto de refrigeración...",
+    descripcion:
+      "Conducto de refrigeración para la pinza de freno trasera, diseñado para mejorar el flujo de aire y ayudar a reducir la acumulación de temperatura durante un uso intensivo.",
     detalles: [
       "Compatible con cualquier moto",
       "Diseño compacto",
       "Diseñado para Supermoto"
-    ]
-    enlaceCompra: "https://..."
-  }
+    ],
+    enlaceCompra: "#"
+  },
   {
-    id: 2
+    id: 2,
     nombre: "AR LEVER GUARD",
     precio: 49.99,
-    imagen: "images/ar-lever-guard.png",
+    imagen: "images/ar-brake-duct.png",
     categoria: "AR FACTORY · PROTECTION",
-    descripcion: "Protector de maneta diseñado para conducción deportiva.",
+    descripcion:
+      "Protector de maneta diseñado para conducción deportiva.",
     detalles: [
       "Diseño ligero",
       "Alta resistencia",
       "Diseñado para Supermoto"
     ],
-    enlaceCompra: "https://..."
+    enlaceCompra: "#"
   }
-
 ];
