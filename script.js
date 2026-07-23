8a10ddca12eefe5027917062d8fdb2a7f457fef3
armotard <aitorubio034@gmail.com>
2026-07-23 19:47:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,44 @@ document.querySelectorAll('.nav a').forEach(link => {
     nav.classList.remove('active');
   });
 });
+const contenedorProductos =
+  document.getElementById("lista-productos");
+productos.forEach(producto => {
+  const tarjeta =
+    document.createElement("article");
+  tarjeta.classList.add("product-card");
+  tarjeta.innerHTML = `
+    <div class="product-image">
+      <img 
+        src="${producto.imagen}"
+        alt="${producto.nombre}"
+      >
+    </div>
+    <div class="product-info">
+      <p class="product-category">
+        ${producto.categoria}
+      </p>
+      <h3>
+        ${producto.nombre}
+      </h3>
+      <p>
+        ${producto.descripcion}
+      </p>
+      <div class="product-details">
+        ${producto.detalles.map(detalle => `
+          <span>
+            ✓ ${detalle}
+          </span>
+        `).join("")}
+      </div>
+      <a
+        href="${producto.enlaceCompra}"
+        target="_blank"
+        class="button button-primary"
+      >
+        COMPRAR
+      </a>
+    </div>
+  `;
+  contenedorProductos.appendChild(tarjeta);
+});
