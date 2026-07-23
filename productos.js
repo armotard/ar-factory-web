b4884fddae258bf078662897dba65094bd2c3321
armotard <aitorubio034@gmail.com>
2026-07-23 19:47:47 +0200

    Update productos.js

--- productos.js
+++ productos.js
@@ -5,14 +5,27 @@ const productos = [
     precio: 39.99,
     imagen: "images/ar-brake-duct.png",
     categoria: "AR FACTORY · BRAKE PERFORMANCE",
-    descripcion:
-      "Conducto de refrigeración para la pinza de freno trasera, diseñado para mejorar el flujo de aire y ayudar a reducir la acumulación de temperatura durante un uso intensivo.",
+    descripcion: "Conducto de refrigeración...",
     detalles: [
       "Compatible con cualquier moto",
       "Diseño compacto",
       "Diseñado para Supermoto"
-    ]
-    enlaceCompra:
-      "https://es.wallapop.com/item/llavero-giratorio-con-rodamiento-antiestres-1257879271"
+    ],
+    enlaceCompra: "https://..."
+  },
+  {
+    id: 2,
+    nombre: "AR LEVER GUARD",
+    precio: 49.99,
+    imagen: "images/ar-lever-guard.png",
+    categoria: "AR FACTORY · PROTECTION",
+    descripcion: "Protector de maneta diseñado para conducción deportiva.",
+    detalles: [
+      "Diseño ligero",
+      "Alta resistencia",
+      "Diseñado para Supermoto"
+    ],
+    enlaceCompra: "https://..."
   }
+
 ];
