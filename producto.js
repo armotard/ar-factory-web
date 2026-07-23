ce8811204db7af791f65e7cd96795b48c0dff739
armotard <aitorubio034@gmail.com>
2026-07-23 20:20:59 +0200

    Update producto.js

--- producto.js
+++ producto.js
@@ -22,3 +22,85 @@ const producto =
 
 const contenedor =
   document.getElementById("producto-contenido");
+
+
+// Mostrar la información del producto
+
+if (producto) {
+
+  contenedor.innerHTML = `
+
+    <div class="product-detail-image">
+
+      <img
+        src="${producto.imagen}"
+        alt="${producto.nombre}"
+      >
+
+    </div>
+
+
+    <div class="product-detail-info">
+
+
+      <p class="product-category">
+
+        ${producto.categoria}
+
+      </p>
+
+
+      <h1>
+
+        ${producto.nombre}
+
+      </h1>
+
+
+      <p class="product-detail-description">
+
+        ${producto.descripcion}
+
+      </p>
+
+
+      <div class="product-details">
+
+        ${producto.detalles.map(detalle => `
+
+          <span>
+            ✓ ${detalle}
+          </span>
+
+        `).join("")}
+
+      </div>
+
+
+      <p class="product-price">
+
+        ${producto.precio.toFixed(2)} €
+
+      </p>
+
+
+      <a
+
+        href="${producto.enlaceCompra}"
+
+        target="_blank"
+
+        class="button button-primary"
+
+      >
+
+        COMPRAR
+
+      </a>
+
+
+    </div>
+
+  `;
+
+}
