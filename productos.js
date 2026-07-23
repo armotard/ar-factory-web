b171b3844faf2145ed3fb812a7fcf9be6c569637
armotard <aitorubio034@gmail.com>
2026-07-23 21:35:06 +0200

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,51 +1,86 @@
 const productos = [
+
   {
     id: 1,
+
     nombre: "AR BRAKE DUCT",
+
     precio: 39.99,
+
     imagenes: [
-  "images/ar-brake-duct.png",
-  "images/ar-brake-duct-2.png",
-  "images/ar-brake-duct-3.png"
-],
+      "images/ar-brake-duct.png"
+    ],
+
     categoria: "AR FACTORY · BRAKE PERFORMANCE",
+
     descripcion:
       "Conducto de refrigeración para la pinza de freno trasera, diseñado para mejorar el flujo de aire y ayudar a reducir la acumulación de temperatura durante un uso intensivo.",
+
     detalles: [
       "Compatible con cualquier moto",
       "Diseño compacto",
       "Diseñado para Supermoto"
     ],
-    enlaceCompra: "https://es.wallapop.com/item/llavero-giratorio-con-rodamiento-antiestres-1257879271"
+
+    enlaceCompra:
+      "https://es.wallapop.com/item/llavero-giratorio-con-rodamiento-antiestres-1257879271"
+
   },
+
+
   {
     id: 2,
+
     nombre: "AR LEVER GUARD",
+
     precio: 49.99,
-    imagen: "images/ar-brake-duct.png",
+
+    imagenes: [
+      "images/ar-brake-duct.png"
+    ],
+
     categoria: "AR FACTORY · PROTECTION",
+
     descripcion:
       "Protector de maneta diseñado para conducción deportiva.",
+
     detalles: [
       "Diseño ligero",
       "Alta resistencia",
       "Diseñado para Supermoto"
     ],
-    enlaceCompra: "https://es.wallapop.com/item/neumatico-enduro-987073657"
+
+    enlaceCompra:
+      "https://es.wallapop.com/item/neumatico-enduro-987073657"
+
   },
-    {
+
+
+  {
     id: 3,
+
     nombre: "Organizador cables",
+
     precio: 7.99,
-    imagen: "images/Organizador-de-cables.png",
+
+    imagenes: [
+      "images/Organizador-de-cables.png"
+    ],
+
     categoria: "AR FACTORY · PROTECTION",
+
     descripcion:
-      "Organiza tus cables de la mejor manera",
+      "Organiza tus cables de la mejor manera.",
+
     detalles: [
       "Diseño ligero",
       "Alta resistencia",
       "Diseñado para Supermoto"
     ],
-    enlaceCompra: "https://es.wallapop.com/item/neumatico-enduro-987073657"
+
+    enlaceCompra:
+      "https://es.wallapop.com/item/neumatico-enduro-987073657"
+
   }
+
 ];
