f76d61fe6ecd56ac4143707cae9fdf1aaed00658
armotard <aitorubio034@gmail.com>
2026-07-31 18:57:52 +0200

    Update productos.js

--- productos.js
+++ productos.js
@@ -2,112 +2,143 @@ const productos = [
 
   {
     id: 1,
-
     nombre: "AR BRAKE DUCT",
-
     precio: 39.99,
-
     imagenes: [
       "images/ar-brake-duct.png"
     ],
-
 categoria: "brake-performance",
 categoriaNombre: "BRAKE PERFORMANCE",
-
     descripcion:
       "Conducto de refrigeración para la pinza de freno trasera, diseñado para mejorar el flujo de aire y ayudar a reducir la acumulación de temperatura durante un uso intensivo.",
-
     compatibilidad: [
     "Universal",
     "YZF 450",
     "KTM 450",
     "Husqvarna 450"
 ],
-
     detalles: [
       "Compatible con cualquier moto",
       "Diseño compacto",
       "Diseñado para Supermoto"
     ],
-
     enlaceCompra:
       "https://es.wallapop.com/item/llavero-giratorio-con-rodamiento-antiestres-1257879271"
-
   },
-
-
   {
     id: 2,
-
     nombre: "AR LEVER GUARD",
-
     precio: 49.99,
-
     imagenes: [
       "images/ar-brake-duct.png"
     ],
-
 categoria: "protection",
 categoriaNombre: "PROTECTION",
-
     descripcion:
       "Protector de maneta diseñado para conducción deportiva.",
-
     compatibilidad: [
     "Universal",
     "YZF 450",
     "KTM 450",
     "Husqvarna 450"
 ],
-
     detalles: [
       "Diseño ligero",
       "Alta resistencia",
       "Diseñado para Supermoto"
     ],
-
     enlaceCompra:
       "https://es.wallapop.com/item/neumatico-enduro-987073657"
-
   },
-
-
   {
     id: 3,
-
     nombre: "Organizador cables",
-
     precio: 7.99,
-
     imagenes: [
       "images/Organizador-de-cables.png"
     ],
-
 categoria: "accessories",
 categoriaNombre: "ACCESSORIES",
-
     descripcion:
       "Organiza tus cables del freno delantero de la mejor manera, amplia personalización del color. ",
-
     compatibilidad: [
     "Universal",
     "YZF 450",
     "KTM 450",
     "Husqvarna 450"
 ],
-
     detalles: [
       "Diseño compacto y elegante",
       "Alta resistencia frente a golpes",
       "Diseñado para Supermoto"
     ],
-    
   especificaciones: {
     material: "PLA",
     peso: "35 g",
     fabricacion: "Impresión 3D",
     compatibilidad: "Universal"
-    
+  },
+    enlaceCompra:
+      "https://es.wallapop.com/item/neumatico-enduro-987073657"
+  },
+    {
+    id: 4,
+    nombre: "Intermitentes SuperMotard",
+    precio: 14,99,
+    imagenes: [
+      "images/Organizador-de-cables.png"
+    ],
+categoria: "accessories",
+categoriaNombre: "ACCESSORIES",
+    descripcion:
+      "Organiza tus cables del freno delantero de la mejor manera, amplia personalización del color. ",
+    compatibilidad: [
+    "Universal",
+    "YZF 450",
+    "KTM 450",
+    "Husqvarna 450"
+],
+    detalles: [
+      "Diseño compacto y elegante",
+      "Alta resistencia frente a golpes",
+      "Diseñado para Supermoto"
+    ],
+  especificaciones: {
+    material: "PLA",
+    peso: "35 g",
+    fabricacion: "Impresión 3D",
+    compatibilidad: "Universal"
+  },
+    enlaceCompra:
+      "https://es.wallapop.com/item/neumatico-enduro-987073657"
+  },
+    {
+    id: 5,
+    nombre: "Conducto Aire Freno Delantero",
+    precio: 24,99,
+    imagenes: [
+      "images/Organizador-de-cables.png"
+    ],
+categoria: "accessories",
+categoriaNombre: "ACCESSORIES",
+    descripcion:
+      "Organiza tus cables del freno delantero de la mejor manera, amplia personalización del color. ",
+    compatibilidad: [
+    "Universal",
+    "YZF 450",
+    "KTM 450",
+    "Husqvarna 450"
+],
+    detalles: [
+      "Diseño compacto y elegante",
+      "Alta resistencia frente a golpes",
+      "Diseñado para Supermoto"
+    ],
+  especificaciones: {
+    material: "PLA",
+    peso: "35 g",
+    fabricacion: "Impresión 3D",
+    compatibilidad: "Universal"
   },
     enlaceCompra:
       "https://es.wallapop.com/item/neumatico-enduro-987073657"
