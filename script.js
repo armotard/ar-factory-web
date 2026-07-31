48c8a628f986e7d40e582dc24dfdf7a4b563bba9
armotard <aitorubio034@gmail.com>
2026-07-31 18:52:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,33 @@ productos.forEach(producto => {
   `;
   contenedorProductos.appendChild(tarjeta);
 });
+const backToTop =
+document.getElementById("backToTop");
+
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>500){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+
+backToTop.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
